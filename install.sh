97f392444f236fe612c54b6fb0cf7763789578ba
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2022-02-10 17:41:42 +0100

    migrate script fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "--- installing frontend dependencies ---"
 docker exec app.frontend bash -c "composer i; php artisan storage:link; chmod -R 777 storage"
 
 echo "--- installing backend dependencies ---"
-docker exec app.backend bash -c "composer i; php artisan migrate; chmod -R 777 storage"
+docker exec app.backend bash -c "composer i; yes yes | php artisan migrate; echo  chmod -R 777 storage"
 
 echo "--- copy env ---"
 cd frontend && cp .env.example .env
