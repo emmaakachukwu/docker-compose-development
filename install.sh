04b29c79b4326ef42841c77ad2d6139d7a5303e0
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2022-02-10 17:18:14 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo "--- building services ---"
 docker-compose up -d
 
-docker exec app.frontend composer i && php artisan storage:link && chmod -R 777 storage
-docker exec app.backend composer i && php artisan migrate && chmod -R 777 storage
+echo "--- installing frontend dependencies ---"
+docker exec app.frontend bash -c "composer i; php artisan storage:link; chmod -R 777 storage"
 
+echo "--- installing backend dependencies ---"
+docker exec app.backend bash -c "composer i; php artisan migrate; chmod -R 777 storage"
+
+echo "--- copy env ---"
 cd frontend && cp .env.example .env
-cd backend && cp .env.example .env
+cd ../backend && cp .env.example .env
+
+echo "done.. open browser at http://0.0.0.0:8088"
